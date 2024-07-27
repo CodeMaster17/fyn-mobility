01c1688aaa8353639119b0bc2f2d29a0252e3e74
NaturesClub <2105891@kiit.ac.in>
2024-07-27 21:58:58 +0530

    feat: create image gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ImageGallery from './components/ImageGallery'
 import Input from './components/Input'
 import SortableList from './components/SortableList'
 
@@ -8,8 +9,11 @@ function App() {
   return (
     <main className='main'>
       <p className='heading'>Fyn Mobility Assignment</p>
+      <p className='heading'>Task 1</p>
       <Input />
       <SortableList />
+      <p className='heading'>Task 2</p>
+      <ImageGallery />
     </main>
   )
 }
---
+++ src/components/ImageGallery.tsx
@@ -0,0 +1,11 @@
+
+
+type Props = {}
+
+const ImageGallery = (props: Props) => {
+    return (
+        <div>ImageGallery</div>
+    )
+}
+
+export default ImageGallery
